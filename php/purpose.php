6e31b4f90b15dc8a213f6b0981dcad3a3238208e
Zachary Fuller <zfuller@chestnut.cs.colostate.edu>
2015-04-27 15:28:23 -0600

    added purpose.php

---
+++ php/purpose.php
@@ -0,0 +1,8 @@
+<?php header('Content-Type: text/json');
+?>
+<script>
+var JSONObject = {
+	"purpose" : "To bring people together for 1337ness"
+	};
+document.getElementById("purpose").innerHTML=JSONObject.purpose;
+</script>
