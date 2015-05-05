95aadd2659bcedf882f956c4a466cee1e75e606f
Zachary Fuller <zfuller@cashew.cs.colostate.edu>
2015-05-04 21:30:34 -0600

    added federation.js

---
+++ ajax/federation.js
@@ -0,0 +1,99 @@
+var url = 'http://www.cs.colostate.edu/~ct310/yr2015sp/project/roster.php?key=WQT3xKmVV7';
+var http = false;
+var id;
+var sites;
+
+if (navigator.appName == "Microsoft Internet Explorer"){
+	http = new ActiveXObject("Microsoft.XMLHTTP");
+	p = new ActiveXObject("Microsoft.XMLHTTP");
+} else {
+	http = new XMLHttpRequest();
+	p = new XMLHttpRequest();
+}
+
+/*NOT TESTED*/
+function getSites(){
+	http.open("POST", url, true);
+	http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	http.onreadystatechange = function() {
+		if (http.readyState == 4){
+			sites = JSON.parse(http.responseText);
+			sitesToTable(sites);
+		}
+	}
+	http.send(null);
+}
+
+/*NOT TESTED*/
+function sitesToTable(sites) {
+	var tab = document.getElementById('federation');
+	var i = tab.rows.length;
+	for (j = 0; j < sites.length; j++){
+		var addr = sites[j].url;
+		var last = addr.lastIndexOf("/");
+		addr = addr.substring(0, last+1);
+		var rt = "<tr> <td> " + sites[j].shortname + " </td> <td>" + sites[j].longname + "</td> <td> <a href='"+addr+"'>" + addr + "</a></td> </tr>";
+		var rr = tab.insertRow(i);
+		rr.innerHTML = rt;	
+	}
+	if (loggedon){
+		hover();
+	}
+}
+
+/*NOT TESTED*/
+function hover(){
+    $(document).ready(function(){
+    	$("th").hover(function(){ 		
+    	});
+		$("tr").hover(function(){
+			id = $(this).find("td:nth-child(3)").text();
+		    getPurpose();
+		    $("#purpose").toggle(1);
+		    document.getElementById('purpose').deleteRow(1);
+		});
+		$("tr").mouseleave(function(){
+			leave();
+		});
+		$("table").mouseleave(function(){
+			leave();
+		});
+	});
+}
+
+function leave(){
+	document.getElementById('purpose').deleteRow(1);
+	document.getElementById('purpose').deleteRow(2);
+}
+/*NOT TESTED*/
+function getPurpose(){
+	var sites;
+	id += "purpose.php";
+	http.open("POST", id, true);
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.onreadystatechange = function() {
+		if (http.readyState == 4) {
+			sites = JSON.parse(http.responseText);
+			if(Array.isArray(sites)){
+				purposeToTable(sites[0].purpose);
+			}else{
+				purposeToTable(sites.purpose);
+			}
+		}
+	}
+	http.send(null);
+}
+
+/*NOT TESTED*/
+function purposeToTable(purpose){
+	var row;
+	row = document.createElement('tr');
+	var cell = document.createElement('td');
+	cell.innerHTML = purpose;
+	row.appendChild(cell);
+	document.getElementById('purpose').appendChild(row);	
+}
+
+function init() {
+	getSites();
+}
--- php/inc/header.php
+++ php/inc/header.php
@@ -9,11 +9,7 @@ $dbh = new DBHelper();
 <html lang="en">
 
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js'></script>
-<script type="text/javascript" src='federation.js'></script>
-<script type="text/javascript">	
-   		window.onload = init;
-   		var loggedon = '1';
-	</script>
+
 
 <head>
     <meta charset="UTF-8">
