194b8f116ab64f62189c81b4e0383eb7d05fcd00
Zachary Fuller <zfuller@cashew.cs.colostate.edu>
2015-05-07 22:48:26 -0600

    fixed purpose.php

--- purpose.php
+++ purpose.php
@@ -1,10 +1,7 @@
 <?php 
-header("Access-Control-Allow-Origin: *"); 
-header('Content-Type: text/json');
+	header("Access-Control-Allow-Origin: *");
+	header('Content-Type: text/json');
+
+	$sitePurpose=array("purpose"=>"To bring people together for 1337ness");
+	echo json_encode($sitePurpose);
 ?>
-<script>
-var JSONObject = {
-	purpose : "To bring people together for 1337ness"
-	};
-document.getElementById("purpose").innerHTML=JSONObject.purpose;
-</script>
